e45ad9495daaf2979cd4b8bc8a72bbab6a417590
Filipe Luis dos Santos <filipe.luisdossantos@gmail.com>
2022-07-30 10:56:20 -0300

    atualizando projeto Pizza

--- NovoProjetoPizza/script.js
+++ NovoProjetoPizza/script.js
@@ -2,5 +2,20 @@ pizzaJson.map((item, index)=> {
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
 
 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click',(e)=>{
+        e.preventDefault();
+
+        document.querySelector('.pizzaWindowArea').style.opacity = '0';
+        document.querySelector('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            document.querySelector('.pizzaWindowArea').style.opacity = '1';
+        },200);
+        
+    });
+
     document.querySelector('.pizza-area').append(pizzaItem);
 });
