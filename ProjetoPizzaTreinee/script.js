14b70207e4eb92ca7fe09b3374906e4a1b2abdd9
Filipe Luis dos Santos <filipe.luisdossantos@gmail.com>
2022-10-20 11:32:08 -0300

    funções do modal criadas

--- ProjetoPizzaTreinee/script.js
+++ ProjetoPizzaTreinee/script.js
@@ -1,6 +1,7 @@
 let pizzaIndex;
 let priceSelected;
 let modalQt;
+let cart = [];
 
 pizzaJson.map((item, index)=>{
     let pizzaItem = document.querySelector('.pizza-item').cloneNode(true);
@@ -40,3 +41,74 @@ pizzaJson.map((item, index)=>{
 });
 
 //adicionando eventos do modal
+
+//seleção de tamanho
+
+document.querySelectorAll('.pizzaInfo--size').forEach((itemSize,indexSize)=>{
+    itemSize.addEventListener('click',()=>{
+        document.querySelector('.pizzaInfo--size.selected').classList.remove('selected');
+        itemSize.classList.add('selected');
+        priceSelected = pizzaJson[pizzaIndex].price[indexSize].toFixed(2);
+        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${priceSelected}`;
+    });
+});
+
+//Seleção de quantidade.
+
+document.querySelector('.pizzaInfo--qtmenos').addEventListener('click',()=>{
+    if(modalQt > 1){
+        modalQt--;
+        document.querySelector('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+document.querySelector('.pizzaInfo--qtmais').addEventListener('click',()=>{
+    modalQt++;
+    document.querySelector('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+//botão cancelar
+
+document.querySelectorAll('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+});
+
+function closeModal() {
+    document.querySelector('.pizzaWindowArea').style.opacity = '0';
+    setTimeout(()=>{
+        document.querySelector('.pizzaWindowArea').style.display = 'none';
+    },500);
+    
+    window.scrollTo({
+        top:0,
+        behavior: "smooth"
+    });
+}
+
+//botão adicionar ao carrinho
+document.querySelector('.pizzaInfo--addButton').addEventListener('click',()=>{
+    let size = document.querySelector('.pizzaInfo--size.selected').getAttribute('data-key');
+    let key = pizzaJson[pizzaIndex].id + '*' + size;
+    let i = cart.findIndex((item)=>item.key == key);
+
+    if (i > -1) {
+        cart[i].qt+= modalQt;
+    } else {
+        cart.push({
+            key:key,
+            id:pizzaJson[pizzaIndex].id,
+            size:size,
+            price:priceSelected,
+            qt:modalQt
+        });
+    }
+    closeModal();
+    updateCart();
+});
+
+function updateCart() {
+    if(cart.length > 0){
+        document.querySelector('aside').classList.add('show');
+    }else {
+        document.querySelector('aside').classList.remove('show');
+    }
+}
