05ebe1dce7e36f07abea8817de2fd8f4ccd1bade
Filipe Luis dos Santos <filipe.luisdossantos@gmail.com>
2022-11-15 10:01:47 -0300

    update

--- UrnaTreino8/script.js
+++ UrnaTreino8/script.js
@@ -126,15 +126,16 @@ function confirma() {
         });
     }
     if(votoConfirmado) {
+        audio.src = 'assets/media/votacao.mp3';
+        audio.play();
         etapaAtual++;
         if(etapas[etapaAtual] !== undefined) {
-            audio.src = 'assets/media/votacao.mp3';
-            audio.play();
             comecarEtapa();
         } else {
             audio.src = 'assets/media/fim.mp3';
             audio.play();
             document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
+            console.log(votos);
         }
         
     }
