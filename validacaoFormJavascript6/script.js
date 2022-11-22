b801c3b5ceb882c618000d6336129970b85fea03
Filipe Luis dos Santos <filipe.luisdossantos@gmail.com>
2022-11-22 10:24:42 -0300

    udpate

--- validacaoFormJavascript6/script.js
+++ validacaoFormJavascript6/script.js
@@ -1,17 +1,17 @@
 let validator = {
-    handleSubmit:(e)=> {
+    handleSubmit:(e)=>{
         e.preventDefault();
         let send = true;
         let inputs = form.querySelectorAll('input');
 
         validator.clearErrors();
 
-        for(let i = 0; i < inputs.length; i++) {
+        for(let i=0; i < inputs.length; i++){
             let input = inputs[i];
             let check = validator.checkInput(input);
             if(check !== true) {
                 send = false;
-                validator.showerror(input, check);
+                validator.showError(input, check);
             }
         }
         if(send){
@@ -22,25 +22,25 @@ let validator = {
         let rules = input.getAttribute('data-rules');
         if(rules !== null) {
             rules = rules.split('|');
-            for( let i in rules) {
+            for(let i in rules) {
                 let rDetails = rules[i].split('=');
                 switch(rDetails[0]) {
                     case 'required':
-                        if(input.value == '') {
+                        if(input.value == ''){
                             return 'Campo obrigatório!';
                         }
                     break;
                     case 'min':
                         if(input.value.length < rDetails[1]) {
-                            return 'Campo precisa ter no mínimo '+rDetails[1]+ ' caracteres!';
+                            return 'Deve ter no mínimo '+rDetails[1]+' caracteres!';
                         }
                     break;
                     case 'email':
-                        if(input.value != '') {
+                        if(input.value !== ''){
                             let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-                            if(!regex.test(input.value.toLowerCase())) {
-                                return 'E-mail digitado não é válido!';
+                            if(!regex.test(input.value.toLowerCase())){
+                                return 'Digite um e-mail válido!';
                             }
                         }
                     break;
@@ -49,13 +49,13 @@ let validator = {
         }
         return true;
     },
-    showerror:(input, error)=>{
-        input.style.borderColor = '#f55454';
-        let errorElement = document.createElement('div');
-        errorElement.classList.add('error');
-        errorElement.innerHTML = error;
+    showError:(input, erro)=>{
+        input.style.borderColor = '#ff0000';
+        let erroElement = document.createElement('div');
+        erroElement.classList.add('error');
+        erroElement.innerHTML = erro;
 
-        input.parentElement.insertBefore(errorElement, input.nextElementSibling);
+        input.parentElement.insertBefore(erroElement, input.nextElement);
     },
     clearErrors:()=>{
         let inputs = form.querySelectorAll('input');
@@ -64,12 +64,12 @@ let validator = {
         }
 
         let erros = form.querySelectorAll('.error');
-        for(let i = 0; i < erros.length; i++) {
+        for(let i=0; i < erros.length; i++) {
             erros[i].remove();
         }
     }
 }
 
 
-let form = document.querySelector('.B7Validator');
+let form = document.querySelector('.validatorjs');
 form.addEventListener('submit', validator.handleSubmit);
